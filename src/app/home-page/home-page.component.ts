b4c5e4310273b2bf0e95e67e86671f7ced4d83e3
SreeshaKp <140167779+SreeshaKp@users.noreply.github.com>
2024-07-17 23:03:37 +0530

    Update home-page.component.ts

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -10,6 +10,30 @@ export class HomePageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const images = [
+      './assets/skyview.png',
+      './assets/snow.jpg',  
+      './assets/travel Slide.png',
+    ];
+    
+    let currentIndex = 0;
+    const sliderImage = document.getElementById('slider-image') as HTMLImageElement;
+    const prevButton = document.getElementById('prev-button') as HTMLButtonElement;
+    const nextButton = document.getElementById('next-button') as HTMLButtonElement;
+    
+    function updateImage() {
+      sliderImage.src = images[currentIndex];
+    }
+    
+    prevButton.addEventListener('click', () => {
+      currentIndex = (currentIndex > 0) ? currentIndex - 1 : images.length - 1;
+      updateImage();
+    });
+    
+    nextButton.addEventListener('click', () => {
+      currentIndex = (currentIndex < images.length - 1) ? currentIndex + 1 : 0;
+      updateImage();
+    });
+    updateImage();
   }
-
 }
